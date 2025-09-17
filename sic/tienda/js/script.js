2a3c7fb102c419bfe092a1abcd020a01de9dd971
Stivii <169058006+Aiimon@users.noreply.github.com>
2025-09-17 10:55:37 -0300

    up

--- sic/tienda/js/script.js
+++ sic/tienda/js/script.js
@@ -18,7 +18,7 @@ window.actualizarNavbarUsuario = () => {
       }
       if (text.includes("perfil")) {
         item.style.display = "block";
-        item.href = "/sic/tienda/perfil.html"; // ruta absoluta
+        item.href = "../sic/tienda/perfil.html"; // ruta absoluta
       }
       if (item.id === "btnLogout") {
         item.style.display = "block";
@@ -38,7 +38,7 @@ window.actualizarNavbarUsuario = () => {
       const text = item.textContent.toLowerCase();
       if (text.includes("ingresar") || text.includes("registrar")) {
         item.style.display = "block";
-        item.href = "/sic/tienda/acceso.html"; // ruta absoluta
+        item.href = "../sic/tienda/acceso.html"; // ruta absoluta
       }
       if (item.id === "btnLogout") item.style.display = "none";
       if (text.includes("perfil")) item.style.display = "none";
